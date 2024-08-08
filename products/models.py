4ec6c7c7a86ea9eac6f300fd9eed5d99498b7f59
lai0xn <onigenko12@gmail.com>
2024-08-08 14:28:45 +0100

    reviews

--- products/models.py
+++ products/models.py
@@ -1,14 +1,20 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
 
 CATEGORIES = {"Abaya":"Abaya","Planner":"Planner"}
+
+
+USER_MODEL = get_user_model()
 
 class Size(models.Model):
     size = models.CharField(max_length=10)
    
 class Color(models.Model):
     color = models.CharField(max_length=10)
+
+
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
@@ -17,3 +23,10 @@ class Product(models.Model):
     sizes = models.ManyToManyField(Size,null=True,blank=True)
     colors = models.ManyToManyField(Color,null=True,blank=True)
     Price = models.IntegerField(null=False)
+
+
+class Review(models.Model):
+    user = models.ForeignKey(USER_MODEL,on_delete=models.CASCADE)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    stars = models.IntegerField()
+    content = models.TextField()
--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
-from .models import Product,Size,Color
+from .models import Product, Review,Size,Color
 
 class ProductSerializer(serializers.ModelSerializer):
     sizes = serializers.SerializerMethodField()
     colors = serializers.SerializerMethodField()
+    reviews = serializers.SerializerMethodField()
     class Meta:
         model = Product
         fields="__all__"
@@ -14,6 +15,16 @@ class ProductSerializer(serializers.ModelSerializer):
     def get_colors(self,obj):
         return obj.colors.all()
 
+
+    def get_reviews(self,obj):
+        return obj.review_set.all()
+
+
+
+class ReviewSeralizer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = '__all__'
 
 
 class SizeSerializer(serializers.ModelSerializer):
