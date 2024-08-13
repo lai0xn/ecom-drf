127fb71a5d2d0d718ac529cd8dee96c9e51b0996
lai0xn <onigenko12@gmail.com>
2024-08-13 11:12:16 +0100

    staticfiles

--- ecom/settings.py
+++ ecom/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from django.conf.global_settings import AUTHENTICATION_BACKENDS
@@ -155,6 +156,9 @@ JWT_SECRET = "ffkkjwyy223!@@#$@$FASFFa"
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
