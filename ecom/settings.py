f07576c490f8f13f7b06833b96b7cc711b492fd5
lai0xn <onigenko12@gmail.com>
2024-09-04 23:31:42 +0100

    finishing up

--- ecom/settings.py
+++ ecom/settings.py
@@ -95,7 +95,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/data/db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
