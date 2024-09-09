dd5c95a3826241da932b318901aed810618347b2
lai0xn <onigenko12@gmail.com>
2024-09-09 15:46:36 +0100

    [feat]: added endpoints

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 
 from products.views import add_email
-from .views import ColorViewSet, MediaViewSet, ProductViewSet, ReviewViewSet, SizeViewSet, add_to_wishlist, get_wishlist, remove_from_wishlist
+from .views import ColorViewSet, CouponViewSet, MediaViewSet, ProductViewSet, ReviewViewSet, SizeViewSet, add_to_wishlist, check_coupon, get_wishlist, remove_from_wishlist
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
@@ -11,11 +11,12 @@ router.register("sizes",SizeViewSet,basename="sizes")
 router.register("colors",ColorViewSet,basename="colors")
 router.register("reviews",ReviewViewSet,basename="reviews")
 router.register("media",MediaViewSet,basename="media")
-
+router.register("coupons",CouponViewSet,basename="coupons")
 urlpatterns = [
     path("",include(router.urls)),
     path("wishlist/add/<int:id>",add_to_wishlist,name="add-wishlist"),
     path("wishlist/remove/<int:id>",remove_from_wishlist,name="remove-wishlist"),
     path("wishlist",get_wishlist,name="get-wishlist"),
+    path("coupons/check",check_coupon,name="check-coupon"),
     path("mail/add",add_email,name="add-email")
 ]
