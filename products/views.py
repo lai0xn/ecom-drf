0494977ca1fbdaf1c0272de42e44beb81ee9a3ee
lai0xn <onigenko12@gmail.com>
2024-09-29 15:08:43 +0100

    recommendations fix

--- products/views.py
+++ products/views.py
@@ -116,9 +116,11 @@ def add_email(request):
 @api_view(["GET"])
 def get_recommended(request):
     number = request.GET.get("number")
-    try:
-        products = Product.objects.all()[:number]
+    products = Product.objects.reverse().all()
+    if len(products) > number:
+
+        serializer = ProductSerializer(products[-number:])
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    else:
         serializer = ProductSerializer(products)
         return Response(serializer.data,status=status.HTTP_200_OK)
-    except Exception:
-        return Response("No products",status=status.HTTP_500_INTERNAL_SERVER_ERROR)
